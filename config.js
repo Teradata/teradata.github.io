55fe596ba56172594ea9d2dca1342dd5a7f5930c
jeremy.smartt@teradata.com <jeremy.smartt@teradata.com>
2018-10-08 10:02:42 -0700

    master update

---
+++ config.js
@@ -0,0 +1,7 @@
+var appConfig = {
+  APP_CENTER_URL: 'https://appcenter.ps.ac.uda.io',
+  APP_CENTER_SERVICES_URL: '/api/app',
+  USERS_SERVICES_URL: '/api/user',
+  SYSTEM_SERVICES_URL: '/api/system',
+  QUERY_SERVICES_URL: '/api/query',
+};
